f7a39556fa8baee1a0d4e5cad897fe034bc1f86c
David Conner <dconner.pro@gmail.com>
2013-09-02 03:21:47 -0600

    macros: this is too easy

--- src/koans/17_macros.clj
+++ src/koans/17_macros.clj
@@ -11,13 +11,13 @@
 
 (defmacro r-infix [form]
   (cond (not (seq? form))
-        __
+        form
         (= 1 (count form))
         `(r-infix ~(first form))
         :else
         (let [operator (second form)
               first-arg (first form)
-              others __]
+              others (nthrest form 2)]
           `(~operator
             (r-infix ~first-arg)
             (r-infix ~others)))))
@@ -36,7 +36,7 @@
   (= '(* 10 2) (macroexpand '(infix-better (10 * 2))))
 
   "Things don't always work as you would like them to... "
-  (= '(+ 10 (2 * 3)) (macroexpand '(infix-better ( 10 + (2 * 3)))))
+  (= '(+ 10 (2 * 3)) (macroexpand '(infix-better ( 10 + (2 * 3))))) ;;......
 
-  "Really, you don't understand recursion until you understand recursion"
+  "Really, you don't really understand recursion until you really understand recursion, really."
   (= 36 (r-infix (10 + (2 * 3) + (4 * 5)))))
